7a4625d6352c9861e46f252d47eb17be8f2fafd6
Gabriel Henrique <gabrielhenriquerr02@gmail.com>
2026-03-26 09:43:03 -0300

    Add files via upload

---
+++ LAB-reserva-BEA/script.js
@@ -0,0 +1,36 @@
+/*function reservar() {
+  let professor = document.getElementById("professor").value;
+  let data = document.getElementById("data").value;
+  let horario = document.getElementById("horario").value;
+
+  let texto = professor + " - " + data + " - " + horario;
+
+  let li = document.createElement("li");
+  li.innerText = texto;
+
+  document.getElementById("lista").appendChild(li);
+}*/
+
+let reservas = [];
+
+function reservar() {
+  let professor = document.getElementById("professor").value;
+  let data = document.getElementById("data").value;
+  let horario = document.getElementById("horario").value;
+
+  let novaReserva = data + "-" + horario;
+
+  for (let r of reservas) {
+    if (r == novaReserva) {
+      alert("Horário já reservado");
+      return;
+    }
+  }
+
+  reservas.push(novaReserva);
+
+  let texto = professor + " - " + data + " - " + horario;
+  let li = document.createElement("li");
+  li.innerText = texto;
+  document.getElementById("lista").appendChild(li);
+}
